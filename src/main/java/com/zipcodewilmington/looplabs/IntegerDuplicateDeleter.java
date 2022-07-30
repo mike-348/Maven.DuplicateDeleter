48eaf64e20fa5ee3d40517c60a40df243dd48f38
Mike <mike@ZipcodersMBP41.fios-router.home>
2022-07-29 20:09:23 -0400

    made a frequency array

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -11,6 +11,23 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
 
     @Override
     public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        Integer[] freq = new Integer[array.length];
+        int visited = -1;
+
+        for (int i = 0; i < array.length; i++) {
+            int count = 1;
+            for (int j = i +1; i< array.length; j++) {
+                if (array[i] == array[j]) {
+                    count++;
+                    freq[j] = visited;
+                }
+            }
+            if (freq[i] != visited) {
+                freq[i] = count;
+            }
+            // now we have a frequency array;
+        }
+
         return new Integer[0];
     }
 
