cb051a5ff63aae1d10fac4246295da2a1ae5e5fb
Mike <mike@Zipcoders-MacBook-Pro-41.local>
2022-08-02 09:56:36 -0400

    lab complete

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -11,28 +11,111 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
 
     @Override
     public Integer[] removeDuplicates(int maxNumberOfDuplications) {
-        Integer[] freq = new Integer[array.length];
-        int visited = -1;
+        Integer[] visited = new Integer[array.length];
+        Integer[] numbers = new Integer[array.length];
+        Integer[] counts = new Integer[array.length];
+        int result_size = array.length;
+        int num_unique_elements = 0;
 
-        for (int i = 0; i < array.length; i++) {
-            int count = 1;
-            for (int j = i +1; i< array.length; j++) {
-                if (array[i] == array[j]) {
-                    count++;
-                    freq[j] = visited;
-                }
-            }
-            if (freq[i] != visited) {
-                freq[i] = count;
-            }
-            // now we have a frequency array;
+        for (int i = 0; i < array.length; i++){
+            visited[i] = 0;
+            numbers[i] = 0;
+            counts[i] = 0;
         }
 
-        return new Integer[0];
+        for (int i = 0; i < array.length; i++) {
+            if (visited[i] == 1){
+                continue;
+            }
+            int value = array[i];
+            visited[i] = 1;
+            numbers[num_unique_elements] = value;
+            counts[num_unique_elements] = 1;
+            for (int j = i + 1; j < array.length; j++) {
+                int new_value = array[j];
+                if (visited[j] == 1){
+                    continue;
+                }
+                if (new_value == value){
+                    visited[j] = 1;
+                    counts[num_unique_elements] += 1;
+                }
+            }
+            if (maxNumberOfDuplications <= counts[num_unique_elements]){
+                result_size -= counts[num_unique_elements];
+            }
+            num_unique_elements += 1;
+
+
+
+        }
+        Integer[] result = new Integer[result_size];
+        int index = 0;
+        for (int i = 0; i < counts.length; i++){
+            if (counts[i] < maxNumberOfDuplications){
+                for (int j = 0; j < counts[i]; j++){
+                    result[index] = numbers[i];
+                    index++;
+                }
+            }
+        }
+
+
+        return result;
     }
 
     @Override
     public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new Integer[0];
+        Integer[] visited = new Integer[array.length];
+        Integer[] numbers = new Integer[array.length];
+        Integer[] counts = new Integer[array.length];
+        int result_size = array.length;
+        int num_unique_elements = 0;
+
+        for (int i = 0; i < array.length; i++){
+            visited[i] = 0;
+            numbers[i] = 0;
+            counts[i] = 0;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (visited[i] == 1){
+                continue;
+            }
+            int value = array[i];
+            visited[i] = 1;
+            numbers[num_unique_elements] = value;
+            counts[num_unique_elements] = 1;
+            for (int j = i +1; j < array.length; j++) {
+                int new_value = array[j];
+                if (visited[j] == 1){
+                    continue;
+                }
+                if (new_value == value){
+                    visited[j] = 1;
+                    counts[num_unique_elements] += 1;
+                }
+            }
+            if (exactNumberOfDuplications == counts[num_unique_elements]){
+                result_size -= exactNumberOfDuplications;
+            }
+            num_unique_elements += 1;
+
+
+            // now we have a frequency array;
+        }
+        Integer[] result = new Integer[result_size];
+        int index = 0;
+        for (int i = 0; i < counts.length; i++){
+            if (counts[i] != exactNumberOfDuplications){
+                for (int j = 0; j < counts[i]; j++){
+                    result[index] = numbers[i];
+                    index++;
+                }
+            }
+        }
+
+
+        return result;
     }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,5 +1,6 @@
 package com.zipcodewilmington.looplabs;
 
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
@@ -11,11 +12,110 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
 
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
+//        return new String[0];
+        Integer[] visited = new Integer[array.length];
+        String[] strings = new String[array.length];
+        Integer[] counts = new Integer[array.length];
+        int result_size = array.length;
+        int num_unique_elements = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            visited[i] = 0;
+            strings[i] = "";
+            counts[i] = 0;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (visited[i] == 1) {
+                continue;
+            }
+            String value = array[i];
+            visited[i] = 1;
+            strings[num_unique_elements] = value;
+            counts[num_unique_elements] = 1;
+            for (int j = i + 1; j < array.length; j++) {
+                String new_value = array[j];
+                if (visited[j] == 1) {
+                    continue;
+                }
+                if (new_value == value) {
+                    visited[j] = 1;
+                    counts[num_unique_elements] += 1;
+                }
+            }
+            if (maxNumberOfDuplications <= counts[num_unique_elements]) {
+                result_size -= counts[num_unique_elements];
+            }
+            num_unique_elements += 1;
+
+
+
+        }
+        String[] result = new String[result_size];
+        int index = 0;
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] < maxNumberOfDuplications) {
+                for (int j = 0; j < counts[i]; j++) {
+                    result[index] = strings[i];
+                    index++;
+                }
+            }
+        }
+
+       return result;
     }
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
+        Integer[] visited = new Integer[array.length];
+        String[] strings = new String[array.length];
+        Integer[] counts = new Integer[array.length];
+        int result_size = array.length;
+        int num_unique_elements = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            visited[i] = 0;
+            strings[i] = "";
+            counts[i] = 0;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (visited[i] == 1) {
+                continue;
+            }
+            String value = array[i];
+            visited[i] = 1;
+            strings[num_unique_elements] = value;
+            counts[num_unique_elements] = 1;
+            for (int j = i + 1; j < array.length; j++) {
+                String new_value = array[j];
+                if (visited[j] == 1) {
+                    continue;
+                }
+                if (new_value == value) {
+                    visited[j] = 1;
+                    counts[num_unique_elements] += 1;
+                }
+            }
+            if (exactNumberOfDuplications == counts[num_unique_elements]) {
+                result_size -= counts[num_unique_elements];
+            }
+            num_unique_elements += 1;
+
+
+
+        }
+        String[] result = new String[result_size];
+        int index = 0;
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] != exactNumberOfDuplications) {
+                for (int j = 0; j < counts[i]; j++) {
+                    result[index] = strings[i];
+                    index++;
+                }
+            }
+        }
+
+        return result;
     }
 }
